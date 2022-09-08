e790ae3b0983b8d9c06c375ea52768d8636b265c
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-08 04:54:14 -0700

    just in

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main -entry point
+ *
+ * Return: 0 after printing the function
+ */
 
 int main()
-{
-    	puts("Programming is like building a multilingual puzzle\n");
+{	puts("\nProgramming is like building a multilingual puzzle");
 	return (0);
 
 }
