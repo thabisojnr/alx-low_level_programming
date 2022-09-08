da56cde9343d8f6662808550b97d4e19ca1f3b51
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-08 05:00:47 -0700

    jdhj

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,7 @@
-#!/bin/bash
-gcc 5-printf.c
-
 #include <stdio.h>
 
 int main(void){
-
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
