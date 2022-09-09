6797c13c7820d61202e34b71329b5cdc11a0bf87
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-09 09:55:44 -0700

    jsf

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - printing numbers from 0-9 with commas and space between them
+ *
  * Description: using the main function
  * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: 0
@@ -16,7 +17,7 @@ if (c != 57)
 {
 															putchar(',');
 															putchar(' ');
-															}
+}
 }
 putchar('\n');
 return (0);
