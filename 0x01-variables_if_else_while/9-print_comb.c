fd7c73039d526dc504ebeaba738ddf9715992b1a
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-09 10:02:26 -0700

    ksk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,16 @@
  */
 int main(void)
 {
-	int c;
-
-	for (c = 48; c <= 57; c++)
-	{
-		putchar(c);
-		if (c != 57)
-		{
-																		putchar(',');
-																		putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+if (c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return(0);
 }
