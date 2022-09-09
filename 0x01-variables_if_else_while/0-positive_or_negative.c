47b35ac5550bf7bf9546c2e6b57f3ec1556b52bf
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-09 09:09:04 -0700

    ksjsh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - print if the number is postive, zero, or negative
- * 
+ *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
@@ -18,15 +18,15 @@ n = rand() - RAND_MAX / 2;
   /* your code goes there */
 if (n > 0)
 {
-	printf("%i is positive\n", n);
+printf("%i is positive\n", n);
 }
-	else if (n == 0)
+else if (n == 0)
 {
-	printf("%i is zero\n", n);
+printf("%i is zero\n", n);
 }
 else if (n < 0)
 {
-	printf("%i is negative\n", n);
+printf("%i is negative\n", n);
 }
 
 return (0);
