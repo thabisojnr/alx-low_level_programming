344337a553e26cb6f2075977054c2787375bd0cb
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-13 10:19:55 -0700

    ksdk

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,18 +11,17 @@ void jack_bauer(void)
 	int minutes = 0;
 	int hours_remainder;
 	int mins_remainder;
-
+	
 	while (hours <= 23)
 	{
 	while (minutes <= 59)
-	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
 	_putchar(':');
 	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');	
+	_putchar(mins_remainder + '0');
 	minutes++;
 	_putchar('\n');
 	}
@@ -30,3 +29,4 @@ void jack_bauer(void)
 	minutes = 0;
 }
 }
+
