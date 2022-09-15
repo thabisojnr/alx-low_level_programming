bceb135c539f0a663ae51dd486245c52ddb7fda4
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-15 04:29:24 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the for uppercase letter
+ *
+ * Return: Always 0.
+ */ 
+
+int _isupper(int c)
+{
+if (c >= 'A' && c >= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
