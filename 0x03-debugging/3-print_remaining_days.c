7402400081243b14c09719e80e47e79361a69d85
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-15 01:29:28 -0700

    lsds

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 {
-if (month >= 3 && days >= 60)
+if (month >= 3 && day >= 60)
 {
 day++;
 }
@@ -25,7 +25,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02/%02/%04\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
