ecb23ab9cea5ff0faeb4c83597480fb7912d93d0
thabisojnr <ndlovuthabios236@gmail.com>
2022-09-19 10:15:18 -0700

    ksd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,20 @@
 
 void puts_half(char *str)
 {
-	int index, half;
+	int len = 0;
 
-	while (str[index] != '\0')
-		index++;
-
-	half = index / 2;
-
-	if (index % 2 == 1)
-		half++;
-
-	while (half < index)
+	while (*str != '\0')
 	{
-		_putchar(str[half]);
-		half++;
+		len++;
+		str++;
 	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
 	_putchar('\n');
 }
