81870274720a165045e515835210edbae6ea0bf6
thabisojnr <ndlovuthabios236@gmail.com>
2022-10-06 05:53:15 -0700

    malloc memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory pointer to allocate
+ * Return: pointer to allocated memory or normal process termination if error
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc_checked(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Another malloc readme
